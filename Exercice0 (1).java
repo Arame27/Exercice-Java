82e2afb439ce691485556cdf03c56f05678fbe1d
Arame27 <78806739+Arame27@users.noreply.github.com>
2021-03-26 14:55:23 +0000

    Add files via upload

---
+++ Exercice0 (1).java
@@ -0,0 +1,9 @@
+//convertir String to float
+
+public class Exercice0{
+    public static void main(String[] args) {
+        String x = "12.5";
+        float y = Float.parseFloat(x);
+        System.out.println(y);
+    }
+}
